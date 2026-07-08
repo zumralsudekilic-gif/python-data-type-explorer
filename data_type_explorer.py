ff194f6bd2032bb56bfa466e4d5a62d40669e5a1
zumralsudekilic-gif <zumralsudekilic@gmail.com>
2026-07-08 22:24:53 +0300

    Update data_type_explorer.py
    add index selection and data type detection

--- data_type_explorer.py
+++ data_type_explorer.py
@@ -3,3 +3,13 @@ print("     PYTHON DATA TYPE EXPLORER")
 print("=================================")
 
 data = [25, 3.14, "Zümral", True]
+
+print("\nData in the list:")
+print(data)
+
+index=int(input("\nEnter an index (0-3): "))
+
+print("Selected value:", data[index])
+print("Data type:", type(data[index]))
+
+
