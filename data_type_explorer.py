b8e3e6a23d9b65edbeb6c33a26163aefb0a573fd
zumralsudekilic-gif <zumralsudekilic@gmail.com>
2026-07-08 22:38:11 +0300

    Update data_type_explorer.py
    add type description

--- data_type_explorer.py
+++ data_type_explorer.py
@@ -11,5 +11,13 @@ index=int(input("\nEnter an index (0-3): "))
 
 print("Selected value:", data[index])
 print("Data type:", type(data[index]))
+if type(data[index]) == int:
+    print("This is an integer (int).")
+elif type(data[index]) == float:
+    print("This is a float.")
+elif type(data[index]) == str:
+    print("This is a string (str).")
+elif type(data[index]) == bool:
+    print("This is a boolean (bool).")
 
 
